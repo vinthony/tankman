927a12bf6413e777814aeb1adc4de7e823aceba2
vinthony <vinthony@gmail.com>
2015-10-20 16:14:59 +0800

    add tank and grid

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,54 +1,184 @@
 
+#include <iostream>
 
-#include <Gl\glut.h>
+#include <GL\glut.h>
+#include <GL\GLU.h>
+#include <GL\GL.h>
+
+
+
+using namespace std;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const double fov = 60.0;
+const float zNear = 0.1f;
+const float zFar = 100.0f;
+const float radisOfMap = 8.0;
+static float tankX = 0;
+static float tankY = 0;
+
+struct vec3 {
+	GLdouble x;
+	GLdouble y;
+	GLdouble z;
+};
+
+
+
 
 void myDisplay(void)
-
-
-
 {
 
-
-
 	glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	
 
-
-
-	glColor3f(1.0, 0.0, 0.0);
-
+	
+	//grids
 	glBegin(GL_LINES);
-
-	glVertex2i(180, 15);
-
-	glVertex2i(10, 145);
-
+	
+	for (GLfloat i = -radisOfMap; i <= radisOfMap; i = i + 1){ // draw the 
+		glVertex3f(radisOfMap, 0.0f, i);
+		glVertex3f(-radisOfMap, 0.0f, i);
+		glVertex3f(i, 0.0f, radisOfMap);
+		glVertex3f(i, 0.0f, -radisOfMap);
+	}
 	glEnd();
 
 
+	glBegin(GL_QUADS);
+	
+	glColor3f(0.0, 0.7, 0.2);
+	glVertex3f(-1.0f,0.0f,0.0f); //bottom
+	glVertex3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(-1.0f, 0.0f, 4.0f);
+	glVertex3f(1.0f, 0.0f, 4.0f);
 
-	glRectf(-0.5f, -0.5f, 0.5f, 0.5f);
+	glColor3f(0.0, 0.7, 0.2);
+	glVertex3f(-1.0f, 0.0f, 0.0f); //back
+	glVertex3f(1.0f, 0.0f, 0.0f);
+	glColor3f(0.0, 0.3, 0.0);
+	glVertex3f(-1.0f, 0.8f, 0.0f);
+	glVertex3f(1.0f, 0.8f, 0.0f);
+
+	glColor3f(0.0, 0.0, 0.2);
+	glVertex3f(-1.0f, 0.0f, 0.0f); //left 
+	glVertex3f(-1.0f, 0.0f, 4.0f);
+	glColor3f(0.0, 0.0, 0.0);
+	glVertex3f(-1.0f, 0.8f, 3.5f);
+	glVertex3f(-1.0f, 0.8f, 0.0f);
+
+	glColor3f(0.0, 0.3, 0.0);
+	glVertex3f(1.0f, 0.0f, 0.0f); //right
+	glVertex3f(1.0f, 0.0f, 4.0f);
+	glColor3f(0.0, 0.7, 0.2);
+	glVertex3f(1.0f, 0.8f, 3.5f);
+	glVertex3f(1.0f, 0.8f, 0.0f);
+
+	glColor3f(0.0, 0.7, 0.2);
+	glVertex3f(-1.0f, 0.8f, 3.5f); //front
+	glVertex3f(1.0f, 0.8f, 3.5f);
+	glColor3f(0.0, 0.3, 0.0);
+	glVertex3f(1.0f, 0.0f, 4.0f);
+	glVertex3f(-1.0f, 0.0f, 4.0f);
+	
+	glColor3f(0.0, 0.7, 0.2);
+	glVertex3f(-1.0f, 0.8f, 0.0f); //top
+	glVertex3f(1.0f, 0.8f, 0.0f);
+	glVertex3f(1.0f, 0.8f, 3.5f);
+	glVertex3f(-1.0f, 0.8f, 3.5f);
+
+	glColor3f(1.0, 0.7, 0.2);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glVertex3f(-1.0f, 0.8f, 0.0f); //bottom
+	glVertex3f(1.0f, 0.8f, 0.0f);
+	glVertex3f(1.0f, 0.8f, 2.0f);
+	glVertex3f(-1.0f, 0.8f,2.0f);
+
+	glVertex3f(-1.0f, 0.8f, 0.0f); //back
+	glVertex3f(1.0f, 0.8f, 0.0f);
+	glVertex3f(0.5f, 1.8f, 0.5f);
+	glVertex3f(-0.5f, 1.8f, 0.5f);
+
+	glVertex3f(-1.0f, 0.8f, 0.0f); //left 
+	glVertex3f(-1.0f, 0.8f, 2.0f);
+	glVertex3f(-0.5f, 1.8f, 2.0f);
+	glVertex3f(-0.5f, 1.8f, 0.5f);
+
+	glVertex3f(1.0f, 0.8f, 0.0f); //right
+	glVertex3f(1.0f, 0.8f, 2.0f);
+	glVertex3f(0.5f, 1.8f, 2.0f);
+	glVertex3f(0.5f, 1.8f, 0.5f);
+
+	glVertex3f(-1.0f, 0.8f, 2.0f); //front
+	glVertex3f(1.0f, 0.8f, 2.0f);
+	glVertex3f(0.5f, 1.8f, 2.0f);
+	glVertex3f(0.5f, 1.8f, 2.0f);
 
 
+	glVertex3f(-0.5f, 1.8f, 0.5f); //top
+	glVertex3f(0.5f, 1.8f, 0.5f);
+	glVertex3f(0.5f,1.8f, 2.0f);
+	glVertex3f(-0.5f, 1.8f, 2.0f);
 
-	glFlush();
+	glEnd();
+	//tanks
+	GLUquadricObj* qobj = gluNewQuadric();
+	gluCylinder(qobj, 0.2f, 0.2f,2.5f,32,32);
 
 
+	glutSwapBuffers();
+
+}
+
+void myKeyboard(unsigned char key, int x, int y){
+	switch (key)
+	{
+		case 'f':
+			if (tankX < radisOfMap)
+				tankX += 1.0;
+			break;
+		case 'b':
+			if (tankX > - radisOfMap)
+				tankX -= 1.0;
+			break;
+		case 'r':
+			tankX = 0.0;
+			tankY = 0.0;
+			break;
+		default:
+			break;
+	}
+	glutPostRedisplay();
+}
+void myReshape(int width, int height){
+
+}
+
+void myAnimation(void){
 
 }
 
 void init(void)
 
 {
-
-	glClearColor(1.0, 1.0, 1.0, 0.0);
-
-
-
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_SMOOTH);
 	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	float aspect = SCREEN_WIDTH / (float)SCREEN_HEIGHT;
+	gluPerspective(fov,aspect,zNear,zFar);
 
-	gluOrtho2D(0.0, 200.0, 0.0, 150.0);
 
-
+	vec3 camera = {10,10,20};
+	vec3 center = {0,0,0};
+	vec3 up = {0,1,0};
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(camera.x, camera.y, camera.z, center.x, center.y, center.z, up.x, up.y, up.z);
 
 }
 
@@ -64,30 +194,28 @@ int main(int argc, char * argv[])
 
 
 
-	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 
 
 
-	glutInitWindowPosition(50, 100);
+	glutInitWindowPosition(200, 200);
 
 
 
-	glutInitWindowSize(400, 300);
+	glutInitWindowSize(SCREEN_WIDTH,SCREEN_HEIGHT);
 
 
 
-	glutCreateWindow("OpenGL");
-
+	glutCreateWindow("TANK");
 
 
 	init();
 
 
-
 	glutDisplayFunc(&myDisplay);
-
-
-
+	glutReshapeFunc(&myReshape);
+	glutKeyboardFunc(&myKeyboard);
+	glutIdleFunc(&myAnimation);
 	glutMainLoop();
 
 
