194d548774bd279ab5b07e8085a2e505b64fc8ba
vinthony <vinthony@gmail.com>
2015-10-22 16:39:32 +0800

    achieve the request

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -20,13 +20,14 @@ static float rotateCount = 0;
 static float rotateCylinder = 0;
 static float prevPosition = 0.0f;
 bool isWire = false;
+bool isFPS = false;
 
 struct vec3 {
 	GLfloat x;
 	GLfloat y;
 	GLfloat z;
 };
-vec3 camera = { 0.0, 5.0, 10.0 };
+vec3 camera = { 10.0, 2.0, 0.0 };
 vec3 center = { 0, 0, 0 };
 vec3 up = { 0, 1, 0 };
 void drawTankPart1(){
@@ -43,12 +44,12 @@ void drawTankPart1(){
 	glVertex3f(-1.0f, 0.0f, 3.0f);
 	glVertex3f(1.0f, 0.0f, 3.0f);
 
-	glColor3f(0.0, 0.7, 0.2);
+	glColor3f(0.0, 0.3, 0.0);
 	glVertex3f(-1.0f, 0.0f, -1.0f); //back
 	glVertex3f(1.0f, 0.0f, -1.0f);
-	glColor3f(0.0, 0.3, 0.0);
-	glVertex3f(-1.0f, 0.8f, -1.0f);
+	glColor3f(0.0, 0.7, 0.2);
 	glVertex3f(1.0f, 0.8f, -1.0f);
+	glVertex3f(-1.0f, 0.8f, -1.0f);
 
 	glColor3f(0.0, 0.0, 0.2);
 	glVertex3f(-1.0f, 0.0f, -1.0f); //left 
@@ -124,7 +125,7 @@ void drawTankPart2(){
 void drawTankPart3(){
 	GLUquadricObj* qobj = gluNewQuadric();
 	if (isWire) gluQuadricDrawStyle(qobj, GLU_LINE);
-	gluCylinder(qobj, 0.2f, 0.2f, 2.5f, 4, 4);
+	gluCylinder(qobj, 0.2f, 0.2f, 2.5f, 40, 40);
 }
 void drawGrid(){
 	glBegin(GL_LINES);
@@ -140,7 +141,7 @@ void drawGrid(){
 void myDisplay(void)
 {
 
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 	glColor3f(1.0f, 1.0f, 1.0f);
 	
@@ -149,22 +150,20 @@ void myDisplay(void)
 	gluLookAt(camera.x, camera.y, camera.z, center.x, center.y, center.z, up.x, up.y, up.z);
 
 	glBegin(GL_LINES);
+	glColor3f(1.0f, 0.0f, 0.0f);
 	glVertex3f(0.0, 10.0f, 0.0);
 	glVertex3f(0.0, -10.0f, 0.0);
-
+	glColor3f(1.0f, 1.0f, 1.0f);
 	glEnd();
 	//grids
 	glPushMatrix();
 	drawGrid();
 	glPopMatrix();
 
-	
 	glPushMatrix();
 		glTranslatef(0, 0, tankX);
 		drawTankPart1();
 	glPopMatrix();
-	
-
 
 	glPushMatrix();
 	glTranslatef(0 , 0, tankX);
@@ -176,17 +175,17 @@ void myDisplay(void)
 
 	
 	glPushMatrix();
-	/*
-	glTranslatef(0.0f, 1.2f, tankX+1.5f);
+	
+	glTranslatef(0.0f, 1.2f, tankX+1.0f);
 	glRotatef(rotateCylinder, 1.0, 0.0, 0.0);
-	glTranslatef(0.0f, -1.2f, -(tankX+1.5f));
-	*/
+	glTranslatef(0.0f, -1.2f, -(tankX+1.0f));
 
-	glTranslatef(0, 1.5f, tankX+1.5f);
+	glTranslatef(0, 1.2f, tankX);
 	glRotatef(rotateCount, 0.0, 1.0, 0.0); //rotate
-	glTranslatef(0, -1.5f, -(tankX+1.5f));
-	glTranslatef(0, 1.5f, tankX+1.5f);
+	glTranslatef(0, -1.2f, -tankX);
+	glTranslatef(0, 1.2f, tankX+1.0f);
 	drawTankPart3();
+	
 	glPopMatrix();
 	
 	glutSwapBuffers();
@@ -196,48 +195,78 @@ void myDisplay(void)
 void myKeyboard(unsigned char key, int x, int y){
 	switch (key)
 	{
-		case 'f':
-			if (tankX < radisOfMap){
-				prevPosition = tankX;
-				tankX += 1.0;
-			}
-			break;
-		case 'b':
-			if (tankX > -radisOfMap){
-				prevPosition = tankX;
-				tankX -= 1.0;
-			}
-			break;
-		case 'r':
-			tankX = 0.0;
-			tankY = 0.0;
-			rotateCount = 0.0;
-			rotateCylinder = 0.0;
-			prevPosition = 0.0;
-			break;
-		case 'w':
-			isWire = !isWire;
-			break;
-		case 't':
-			rotateCount += 10.0;
-			break;
-		case 'u':
-			rotateCylinder -= 10.0;
-			break;
-		case 'd':
-			rotateCylinder += 10.0;
-			break;
-		case 'h':
-			camera.x += 1.0;
-			break;
-		case 'j':
-			camera.y += 1.0;
-			break;
-		case 'k':
-			camera.z += 1.0;
-			break;
-		case 'l':
-			camera.z -= 1.0f;
+	case 'f':
+		if (tankX < radisOfMap){
+			tankX += 1.0;
+		}
+		if (isFPS){
+			camera.x = 0.0;
+			camera.y = 3.0;
+			camera.z = tankX;
+			center.x = 0.0;
+			center.y = 2.0;
+			center.z = tankX + 3.0;
+		}
+		break;
+	case 'b':
+		if (tankX > -radisOfMap){
+			tankX -= 1.0;
+		}
+		if (isFPS){
+			camera.x = 0.0;
+			camera.y = 3.0;
+			camera.z = tankX;
+			center.x = 0.0;
+			center.y = 2.0;
+			center.z = tankX + 3.0;
+		}
+		break;
+	case 'r':
+		tankX = 0.0;
+		tankY = 0.0;
+		rotateCount = 0.0;
+		rotateCylinder = 0.0;
+		prevPosition = 0.0;
+		break;
+	case 'w':
+		isWire = !isWire;
+		break;
+	case 't':
+		rotateCount += 10.0;
+		break;
+	case 'u':
+		if (rotateCylinder > -20.0){
+			rotateCylinder -= 5.0;
+		}
+		break;
+	case 'd':
+		if (rotateCylinder < 10.0){
+			rotateCylinder += 5.0;
+		}
+		break;
+	case 'h':
+		camera.x += 1.0;
+		break;
+	case 'j':
+		camera.y += 1.0;
+		break;
+	case 'k':
+		camera.z += 1.0;
+		break;
+	case 'l':
+		camera.z -= 1.0f;
+		break;
+	case 'm':
+		isFPS = !isFPS;
+		if (isFPS)
+		{
+			camera.x = 0.0;
+			camera.y = 3.0;
+			camera.z = tankX;
+			center.x = 0.0;
+			center.y = 2.0;
+			center.z = tankX + 3.0;
+		}
 			break;
 		default:
 			break;
@@ -257,6 +286,7 @@ void init(void)
 {
 	// whole init
 	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glEnable(GL_DEPTH_TEST);
 	glShadeModel(GL_SMOOTH);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
